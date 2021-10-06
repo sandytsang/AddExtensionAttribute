0cebca0834e36c2df6e351ca2e956219e9b166cb
Sandy Zeng [MVP] <28437644+sandytsang@users.noreply.github.com>
2021-10-06 15:12:32 +0300

    updated

---
+++ Add-GraphPermissionToMSI.ps1
@@ -0,0 +1,17 @@
+﻿
+# Assign static variables
+$MSIObjectID = "7ba08f0a-1571-4345-a30a-6fb39273fcb6" #change this to your object id
+
+# Authenticate against Azure AD, as Global Administrator
+Connect-AzureAD
+
+$MSGraphAppId = "00000003-0000-0000-c000-000000000000" # Microsoft Graph (graph.microsoft.com) application ID
+$MSGraphServicePrincipal = Get-AzureADServicePrincipal -Filter "appId eq '$($MSGraphAppId)'"
+$RoleNames = @("Device.ReadWrite.All", "GroupMember.Read.All" )
+
+# Assign each roles to Managed System Identity, first validate they exist
+foreach ($RoleName in $RoleNames) {
+    $AppRole = $MSGraphServicePrincipal.AppRoles | Where-Object Value -match "$RoleName" 
+    New-AzureAdServiceAppRoleAssignment -ObjectId $MSIObjectID -PrincipalId $MSIObjectID -ResourceId $MSGraphServicePrincipal.ObjectId -Id $AppRole.Id
+}
+
